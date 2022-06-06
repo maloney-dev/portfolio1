1aeb737ded596fb5e88c8503c22600da219e5865
Nicolas Maloney <nicolasmaloneyf@gmail.com>
2022-06-06 17:25:54 -0300

    correct tbn transiction app.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const allSections= document.querySelector('.main-content');
 function pageTransitions() {
     //Button click #active class
     for(let i=0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', () =>{
+        sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn'
         })
     }
 }
 
-pageTransitions();
+pageTransitions(); 
